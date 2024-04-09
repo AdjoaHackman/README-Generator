ebf983eaf3563e5060156dbd839eac9898ec38bb
Adjoa Hackman <adjoahackman93@gmail.com>
2024-04-08 20:11:07 -0400

    pushing additional changes after some testing

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+  if (license == 'ISC') {
+    return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+  }
+  return 'no license for' + license
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { 
+function renderLicenseLink(license) {
   if (license == 'ISC') {
     return 'https://opensource.org/license/isc-license-txt';
   }
-  return 'no license for' + license 
+  return 'no license for' + license
 }
 
 // TODO: Create a function that returns the license section of README
@@ -18,32 +23,34 @@ function renderLicenseSection(license) {
     return 'Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.';
   }
   return 'no license for' + license;
- }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   licenseSection = renderLicenseSection(data.license)
   licenseLink = renderLicenseLink(data.license)
+  licenseBadge = renderLicenseBadge(data.license)
   return `
-          # Title
-             ${data.title}
-          # Description
-             ${data.description}
-          # Installation
-             ${data.installation}
-          # Usage
-             ${data.usage}
-          # Contribution
-             ${data.contribution}
-          # Testing
-             ${data.testing}
-          # License
-             ${licenseSection}
-             [License Link](${licenseLink})
-          # GitHub
-             ${data.github}
-          # Email
-             ${data.email}
+# Title
+${data.title}
+# Description
+${data.description}
+# Installation
+${data.installation}
+# Usage
+${data.usage}
+# Contribution
+${data.contribution}
+# Testing
+${data.testing}
+# License
+${licenseSection}
+[License Badge](${licenseBadge})
+[License Link](${licenseLink})
+# GitHub
+${data.github}
+# Email
+${data.email}
 `;
 }
 
